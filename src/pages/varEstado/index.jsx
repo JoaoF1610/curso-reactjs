357c62a1cc5af2b67443b8052281cd57e816cf75
joao <joaovictorfarias1610@gmail.com>
2024-09-14 20:50:40 -0300

    update state - arrow funcs

--- src/pages/varEstado/index.jsx
+++ src/pages/varEstado/index.jsx
@@ -2,8 +2,13 @@ import './index.scss';
 import { useState } from 'react';
 
 export default function VarEstado() {
-    
-    const[contador, setContador] = useState(0);
+
+    const [contador, setContador] = useState(0);
+    const [tituloS2, setTituloS2] = useState("Oba");
+    const [tituloS3, setTituloS3] = useState("Escolha um");
+    const [selecionouS4, setSelecionouS4] = useState(true);
+    const [tituloS5, setTituloS5] = useState("Fala aí");
+    const [descricaoS5, setDescricaoS5] = useState("Fala aí");
 
     function aumentaCont() {
         setContador(contador + 1)
@@ -32,6 +37,32 @@ export default function VarEstado() {
 
             </div>
 
+            <div className='secao'>
+                <h1>{tituloS2}</h1>
+                <input onChange={e => setTituloS2(e.target.value)} type="text" value={tituloS2} />
+            </div>
+
+            <div className='secao'>
+                <h1>{tituloS3}</h1>
+                <select onChange={e => setTituloS3(e.target.value)}>
+                    <option>Java</option>
+                    <option>JavaScript</option>
+                    <option>Swift</option>
+                    <option>Python</option>
+                    <option>React</option>
+                </select>
+            </div>
+
+            <div className='secao'>
+                <h1>Tudo bem, meu chapa? {selecionouS4 ? 'Sim' : 'Não'}</h1>
+                <input onChange={e => setSelecionouS4(e.target.checked)} type="checkbox" checked={selecionouS4} /> sim
+            </div>
+
+            <div className='secao'>
+                <h1>{tituloS5}</h1>
+                <input onChange={e => setDescricaoS5(e.target.value)} type="text" value={descricaoS5} />
+                <button onClick={() => setTituloS5(descricaoS5)}>Alterar</button>
+            </div>
 
         </div>
 
