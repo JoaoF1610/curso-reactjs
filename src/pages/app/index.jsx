290b10e06bc90480cb9e3ab06b69b6ce27470a61
joao <joaovictorfarias1610@gmail.com>
2024-09-14 14:46:23 -0300

    update all

--- src/pages/app/index.jsx
+++ src/pages/app/index.jsx
@@ -1,17 +1,34 @@
-import './index.css';
+import './index.scss';
 
 import { Link } from 'react-router-dom';
- 
+
 export default function App() {
   return (
-    <div className="App">
-      <h1>Reaaaact</h1>
-      
-      <ul>
-        <li>
-          <Link to='/contato'> Ir para contato </Link>
-        </li>
-      </ul>
+    <div className="pagina-app pagina">
+      <header className='cabecalho'>
+        <h1 className='titulo1'>Reaaaact</h1>
+      </header>
+
+      <section className='secao'>
+        <h1>Estudando ReactJS</h1>
+
+          <input type="text" placeholder='Digite aqui...'/>
+          <br />
+          <select>
+            <option>Item 1</option>
+            <option>Item 2</option>
+          </select>
+          <br />
+          <button>Clique Aqui</button>
+
+        <ul>
+          <li>
+            <Link to='/contato'> Ir para contato </Link>
+          </li>
+        </ul>
+      </section>
+
+
     </div>
   );
 }
